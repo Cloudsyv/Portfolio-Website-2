d9a261a80cfde4eca01afc212191a8e3dba3fdcb
Izaiah <IzaiahColemanMain@gmail.com>
2026-01-21 14:22:22 -0500

    Set up components

--- portfolio-v2/src/app/page.js
+++ portfolio-v2/src/app/page.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <button className="bg-black text-white p-4 rounded-lg">Test</button>
+    <div className="flex min-h-screen items-center justify-center bg-gray-700 font-serif">
+      <main>
+        <Navbar />
+        <section>HERO stuff</section>
+        <section>PROJECTS</section>
+        <Footer />
+      </main>
+    </div>
   );
-  /*(
+  /*return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
